5db1ebbf7ab7fa024f80fe3b2a4b73cffa945c15
Jan Winkler <winkler@cs.uni-bremen.de>
2015-11-18 16:40:22 +0100

    Apache 2 license

--- include/detect/Headers.h
+++ include/detect/Headers.h
@@ -1,3 +1,20 @@
+// Copyright 2016 Jan Winkler <jan.winkler.84@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/** \author Jan Winkler */
+
+
 #ifndef __HEADERS_H__
 #define __HEADERS_H__
 
@@ -12,7 +29,7 @@ namespace detect {
   struct IPHeader {
     __u8 ihl:4;
     __u8 version:4;
-    __u8	tos;
+    __u8 tos;
     __be16 tot_len;
     __be16 id;
     __be16 frag_off;
