724d4c6075b7077ed94d478e600d5ceab34aa8c9
Jan Winkler <winkler@cs.uni-bremen.de>
2015-11-26 09:08:23 +0100

    Corrected basic python interface such that it works

--- pymacdetect/include/pymacdetect/PyMACDetect.h
+++ pymacdetect/include/pymacdetect/PyMACDetect.h
@@ -6,17 +6,15 @@
 #include <python2.7/Python.h>
 
 
-namespace pymacdetect {
-  static PyObject* macdetectClient(PyObject* pyoSelf, PyObject* pyoArgs);
-  PyMODINIT_FUNC initMACDetect(void);
-  
-  static PyObject* pyoMACDetectError;
-  static PyMethodDef PyMACDetectMethods[] = {
-    {"macdetectClient",  macdetectClient, METH_VARARGS,
-     "Create a MAC Detect client instance."},
-    {NULL, NULL, 0, NULL}
-  };
-}
+static PyObject* macdetectClient(PyObject* pyoSelf, PyObject* pyoArgs);
+PyMODINIT_FUNC initpymacdetect(void);
+
+static PyObject* pyoMACDetectError;
+static PyMethodDef PyMACDetectMethods[] = {
+  {"macdetectClient",  macdetectClient, METH_VARARGS,
+   "Create a MAC Detect client instance."},
+  {NULL, NULL, 0, NULL}
+};
 
 
 #endif /* __PYMACDETECT_H__ */
--- pymacdetect/src/pymacdetect/PyMACDetect.cpp
+++ pymacdetect/src/pymacdetect/PyMACDetect.cpp
@@ -1,18 +1,16 @@
 #include <pymacdetect/PyMACDetect.h>
 
 
-namespace pymacdetect {
-  static PyObject* macdetectClient(PyObject* pyoSelf, PyObject* pyoArgs) {
-    return Py_BuildValue("s", "test");
-  }
+static PyObject* macdetectClient(PyObject* pyoSelf, PyObject* pyoArgs) {
+  return Py_BuildValue("s", "test");
+}
+
+PyMODINIT_FUNC initpymacdetect(void) {
+  PyObject* pyoM = Py_InitModule("pymacdetect", PyMACDetectMethods);
   
-  PyMODINIT_FUNC initMACDetect(void) {
-    PyObject* pyoM = Py_InitModule("pymacdetect", PyMACDetectMethods);
-    
-    if(pyoM) { // Error
-      pyoMACDetectError = PyErr_NewException("pymacdetect.error", NULL, NULL);
-      Py_INCREF(pyoMACDetectError);
-      PyModule_AddObject(pyoM, "error", pyoMACDetectError);
-    }
+  if(pyoM) { // Error
+    pyoMACDetectError = PyErr_NewException("pymacdetect.error", NULL, NULL);
+    Py_INCREF(pyoMACDetectError);
+    PyModule_AddObject(pyoM, "error", pyoMACDetectError);
   }
 }
