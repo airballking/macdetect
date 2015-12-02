0e38601bef58e6b4b5b62b535045c77022e67b14
Jan Winkler <winkler@cs.uni-bremen.de>
2015-12-02 19:51:15 +0100

    Check privileges when starting daemon

--- macdetectd/src/daemon.cpp
+++ macdetectd/src/daemon.cpp
@@ -30,8 +30,6 @@
 #include <macdetect-utils/ArgumentParser.h>
 
 
-//macdetect::Network g_nwNetwork;
-//macdetect::Server g_srvServer;
 macdetect::Daemon g_dmDaemon;
 
 
@@ -39,8 +37,6 @@ void catchHandler(int nSignum) {
   switch(nSignum) {
   case SIGTERM:
   case SIGINT: {
-    //g_nwNetwork.shutdown();
-    //g_srvServer.shutdown();
     g_dmDaemon.shutdown();
   } break;
     
@@ -59,58 +55,64 @@ int main(int argc, char** argv) {
   
   int nReturnvalue = EXIT_FAILURE;
   
-  macdetect::ArgumentParser apArguments({{"d", "daemon", macdetect::ArgumentParser::Switch}});
-  apArguments.parse(argc, argv);
-  bool bRegularStartup = false;
+  if(g_dmDaemon.privilegesSuffice()) {
+    macdetect::ArgumentParser apArguments({{"d", "daemon", macdetect::ArgumentParser::Switch}});
+    apArguments.parse(argc, argv);
+    bool bRegularStartup = false;
   
-  if(apArguments.switched("daemon")) {
-    pid_t pPid = fork();
+    if(apArguments.switched("daemon")) {
+      pid_t pPid = fork();
     
-    if(pPid == 0) {
-      // We are the child.
-      umask(0);
+      if(pPid == 0) {
+	// We are the child.
+	umask(0);
       
-      openlog(argv[0], LOG_NOWAIT | LOG_PID, LOG_USER);
-      syslog(LOG_NOTICE, "Started macdetectd daemon.");
+	openlog(argv[0], LOG_NOWAIT | LOG_PID, LOG_USER);
+	syslog(LOG_NOTICE, "Started macdetectd daemon.");
       
-      pid_t pSid = setsid();
-      if(pSid < 0) {
-	syslog(LOG_ERR, "Could not create process group (failed during `setsid()`).");
-      } else {
-	// NOTE(winkler): One could change the working directory here;
-	// we rely on the local data directory, so we're not doing
-	// that at the moment.
+	pid_t pSid = setsid();
+	if(pSid < 0) {
+	  syslog(LOG_ERR, "Could not create process group (failed during `setsid()`).");
+	} else {
+	  // NOTE(winkler): One could change the working directory here;
+	  // we rely on the local data directory, so we're not doing
+	  // that at the moment.
 	
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
+	  close(STDIN_FILENO);
+	  close(STDOUT_FILENO);
+	  close(STDERR_FILENO);
 	
+	  nReturnvalue = EXIT_SUCCESS;
+	}
+      
+	if(nReturnvalue == EXIT_SUCCESS) {
+	  bRegularStartup = true;
+	} else {
+	  closelog();
+	}
+      } else if(pPid < 0) {
+	// Something went wrong.
+      } else if(pPid > 0) {
+	// We are the parent.
 	nReturnvalue = EXIT_SUCCESS;
       }
-      
-      if(nReturnvalue == EXIT_SUCCESS) {
-	bRegularStartup = true;
-      } else {
-	closelog();
+    } else {
+      bRegularStartup = true;
+    }
+  
+    if(bRegularStartup) {
+      syslog(LOG_NOTICE, "Entering main loop.");
+    
+      while(g_dmDaemon.cycle()) {
+	usleep(10);
       }
-    } else if(pPid < 0) {
-      // Something went wrong.
-    } else if(pPid > 0) {
-      // We are the parent.
-      nReturnvalue = EXIT_SUCCESS;
+    
+      syslog(LOG_NOTICE, "Exiting gracefully.");
+      closelog();
     }
   } else {
-    bRegularStartup = true;
+    syslog(LOG_NOTICE, "User privileges don't suffice. Exiting.");
   }
   
-  if(bRegularStartup) {
-    syslog(LOG_NOTICE, "Entering main loop.");
-    
-    while(g_dmDaemon.cycle()) {
-      usleep(10);
-    }
-    
-    syslog(LOG_NOTICE, "Exiting gracefully.");
-    closelog();
-  }
+  return nReturnvalue;
 }
