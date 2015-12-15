87e75113cdd8fd961a6a9d5992367e5667258ac8
Jan Winkler <winkler@cs.uni-bremen.de>
2015-12-15 20:57:20 +0100

    Removed superfluous test script

--- pymacdetect/scripts/test.py
+++
@@ -1,8 +0,0 @@
-import pymacdetect
-
-mdcliClient = pymacdetect.createClient()
-
-print pymacdetect.connectClient(mdcliClient, "192.168.178.22")#127.0.0.1")
-print pymacdetect.knownMACAddresses(mdcliClient)
-
-pymacdetect.destroyClient(mdcliClient)
