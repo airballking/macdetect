62ae5a9fac11933d209d8cc4f5adc2d66204b176
Jan Winkler <winkler@cs.uni-bremen.de>
2015-12-16 09:57:21 +0100

    Some more config for nosetests

--- pymacdetect/tests.sh
+++ pymacdetect/tests.sh
@@ -3,5 +3,5 @@
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${CWDIR}
-PYTHONPATH=$PYTHONPATH:../lib nosetests --with-coverage --cover-inclusive -v -w "scripts" -w "tests" -e "gi\.*"
+PYTHONPATH=$PYTHONPATH:../lib nosetests --with-coverage --cover-package=pymdLib,pymdGui -v -e "gi\.*"
 cd -
