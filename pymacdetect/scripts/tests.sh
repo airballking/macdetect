3da5e9fbb8324ea8ac025c07ecff1db83870d804
Jan Winkler <winkler@cs.uni-bremen.de>
2015-12-21 09:48:21 +0100

    Always use local files first

--- pymacdetect/scripts/tests.sh
+++ pymacdetect/scripts/tests.sh
@@ -3,5 +3,5 @@
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${CWDIR}
-PYTHONPATH=$PYTHONPATH:../../lib nosetests --with-coverage --cover-package=pymdLib,pymdGui -v -e "gi\.*"
+PYTHONPATH=../../lib:$PYTHONPATH nosetests --with-coverage --cover-package=pymdLib,pymdGui -v -e "gi\.*"
 cd -
