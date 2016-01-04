65d5e3130845a5096abbc94715bf2c1ac0ba6b1f
Jan Winkler <winkler@cs.uni-bremen.de>
2016-01-04 12:56:03 +0100

    Added script for simplified version tagging

---
+++ scripts/tag_version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=$1
+COMMENT=$2
+
+CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# For good measure (and maybe we want to do additional operations
+# later on), go into the base directory of the repository first.
+cd ${CWDIR}/..
+
+git tag -a $1 -m $2
+git push --tags
+
+cd -
