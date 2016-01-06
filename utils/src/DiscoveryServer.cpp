61889f6e6462e5a431bdb3bdc933117209614a60
Jan Winkler <winkler@cs.uni-bremen.de>
2016-01-06 07:28:14 +0100

    Removed debug output

--- utils/src/DiscoveryServer.cpp
+++ utils/src/DiscoveryServer.cpp
@@ -12,7 +12,7 @@ namespace macdetect {
     if(valReceived->key() == "request" && valReceived->content() == "server-info") {
       std::shared_ptr<Value> valResponse = std::make_shared<Value>("response", "server-info");
       valResponse->add("server-name", m_strIdentifier);
-      valResponse->print();
+      
       // TODO(winkler): Set the received sender IP as new receiver of
       // this information
       this->send(valResponse);
